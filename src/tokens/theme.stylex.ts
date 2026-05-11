bcc0e2f2532161fc78be8e24f54974e6e91552ad
Chabib N. <chabib.nr@gmail.com>
2026-05-11 09:03:47 +0700

    fix: invalid colors

--- src/tokens/theme.stylex.ts
+++ src/tokens/theme.stylex.ts
@@ -2,14 +2,16 @@ export { theme }
 
 const theme = defineVars({
   '--sw-text-shadow-color': types.color(
-    'initial',
+    'transparent',
   ) satisfies Types.Color<string>,
-  '--sw-box-shadow-color': types.color('initial') satisfies Types.Color<string>,
+  '--sw-box-shadow-color': types.color(
+    'transparent',
+  ) satisfies Types.Color<string>,
   '--sw-inset-box-shadow-color': types.color(
-    'initial',
+    'transparent',
   ) satisfies Types.Color<string>,
   '--sw-drop-shadow-color': types.color(
-    'initial',
+    'transparent',
   ) satisfies Types.Color<string>,
 })
 
