31b57a26dc7043cda8ce969f02199155c69c6572
Chabib N. <chabib.nr@gmail.com>
2026-05-18 09:35:26 +0700

    refactor: filters

--- src/tokens/filter.stylex.ts
+++ src/tokens/filter.stylex.ts
@@ -1,39 +1,29 @@
-export { blur }
-export { dropShadow }
-export { dropShadowColor }
+export { filter }
 
-const dropShadowColor = defineVars({
-  xs: types.color('rgb(0 0 0 / 0.05)') satisfies Types.Color<string>,
-  sm: types.color('rgb(0 0 0 / 0.15)') satisfies Types.Color<string>,
-  md: types.color('rgb(0 0 0 / 0.12)') satisfies Types.Color<string>,
-  lg: types.color('rgb(0 0 0 / 0.15)') satisfies Types.Color<string>,
-  xl: types.color('rgb(0 0 0 / 0.1)') satisfies Types.Color<string>,
-  '2xl': types.color('rgb(0 0 0 / 0.15)') satisfies Types.Color<string>,
-})
+const dropShadowColor = {
+  xs: 'rgb(0 0 0 / 0.05)',
+  sm: 'rgb(0 0 0 / 0.15)',
+  md: 'rgb(0 0 0 / 0.12)',
+  lg: 'rgb(0 0 0 / 0.15)',
+  xl: 'rgb(0 0 0 / 0.1)',
+  '2xl': 'rgb(0 0 0 / 0.15)',
+} as const
 
-const dropShadow = defineConsts({
-  xs: `drop-shadow(0 1px 1px var(--sw-drop-shadow-color, ${dropShadowColor.xs}))`,
-  sm: `drop-shadow(0 1px 2px var(--sw-drop-shadow-color, ${dropShadowColor.sm}))`,
-  md: `drop-shadow(0 3px 3px var(--sw-drop-shadow-color, ${dropShadowColor.md}))`,
-  lg: `drop-shadow(0 4px 4px var(--sw-drop-shadow-color, ${dropShadowColor.lg}))`,
-  xl: `drop-shadow(0 9px 7px var(--sw-drop-shadow-color, ${dropShadowColor.xl}))`,
-  '2xl': `drop-shadow(0 25px 25px var(--sw-drop-shadow-color, ${dropShadowColor['2xl']}))`,
-} as const)
-
-//
-
-const blur = defineConsts({
-  xs: 'blur(4px)',
-  sm: 'blur(8px)',
-  md: 'blur(12px)',
-  lg: 'blur(16px)',
-  xl: 'blur(24px)',
-  '2xl': 'blur(40px)',
-  '3xl': 'blur(64px)',
+const filter = defineConsts({
+  'drop-shadow-xs': `drop-shadow(0 1px 1px var(--sw-drop-shadow-color, ${dropShadowColor.xs}))`,
+  'drop-shadow-sm': `drop-shadow(0 1px 2px var(--sw-drop-shadow-color, ${dropShadowColor.sm}))`,
+  'drop-shadow-md': `drop-shadow(0 3px 3px var(--sw-drop-shadow-color, ${dropShadowColor.md}))`,
+  'drop-shadow-lg': `drop-shadow(0 4px 4px var(--sw-drop-shadow-color, ${dropShadowColor.lg}))`,
+  'drop-shadow-xl': `drop-shadow(0 9px 7px var(--sw-drop-shadow-color, ${dropShadowColor.xl}))`,
+  'drop-shadow-2xl': `drop-shadow(0 25px 25px var(--sw-drop-shadow-color, ${dropShadowColor['2xl']}))`,
+  'blur-xs': 'blur(4px)',
+  'blur-sm': 'blur(8px)',
+  'blur-md': 'blur(12px)',
+  'blur-lg': 'blur(16px)',
+  'blur-xl': 'blur(24px)',
+  'blur-2xl': 'blur(40px)',
+  'blur-3xl': 'blur(64px)',
 } as const)
 
 import { defineConsts } from '@stylexjs/stylex'
-import { defineVars } from '@stylexjs/stylex'
-import { types } from '@stylexjs/stylex'
-import type { Types } from '@stylexjs/stylex'
 //
