0902c36479868f446ce6880513a111a1cc951b11
Chabib N. <chabib.nr@gmail.com>
2026-05-23 08:46:14 +0700

    test: cleanup

--- src/tokens/box-shadow.stylex.spec.ts
+++ src/tokens/box-shadow.stylex.spec.ts
@@ -4,22 +4,8 @@ describe('boxShadow', () => {
   })
 })
 
-describe('boxShadowColor', () => {
-  it('matches snapshot', () => {
-    expect(boxShadowColor).toMatchSnapshot()
-  })
-})
-
-describe('insetBoxShadow', () => {
-  it('matches snapshot', () => {
-    expect(insetBoxShadow).toMatchSnapshot()
-  })
-})
-
 import { boxShadow } from '#/tokens/box-shadow.stylex'
-import { boxShadowColor } from '#/tokens/box-shadow.stylex'
 import { describe } from 'vitest'
 import { expect } from 'vitest'
-import { insetBoxShadow } from '#/tokens/box-shadow.stylex'
 import { it } from 'vitest'
 //
--- src/tokens/text-shadow.stylex.spec.ts
+++ src/tokens/text-shadow.stylex.spec.ts
@@ -4,15 +4,8 @@ describe('textShadow', () => {
   })
 })
 
-describe('textShadowColor', () => {
-  it('matches snapshot', () => {
-    expect(textShadowColor).toMatchSnapshot()
-  })
-})
-
 import { describe } from 'vitest'
 import { expect } from 'vitest'
 import { it } from 'vitest'
 import { textShadow } from '#/tokens/text-shadow.stylex'
-import { textShadowColor } from '#/tokens/text-shadow.stylex'
 //
