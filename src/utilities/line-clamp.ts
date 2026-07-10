c37515711a92a360ed5bf9ff2eda8589eee7d23e
Chabib N. <chabib.nr@gmail.com>
2026-07-10 16:11:55 +0700

    refactor: line clamp

--- src/utilities/line-clamp.ts
+++ src/utilities/line-clamp.ts
@@ -1,27 +1,30 @@
-export { lineClamp }
+export { lineClamped }
+export { notLineClamped }
 
 import { create } from '@stylexjs/stylex'
 
-const base = {
-  overflow: 'hidden',
-  display: '-webkit-box',
-  WebkitBoxOrient: 'vertical',
-}
+const { lineClamped, notLineClamped } = create({
+  /**
+   * Example:
+   * ```tsx
+   * <div
+   *   styleDeck={[
+   *     [lineClamped, { WebkitLineClamp: 3 }],
+   *     { color: 'red' }
+   *   ]}
+   * />
+   * ```
+   */
+  lineClamped: {
+    overflow: 'hidden',
+    WebkitBoxOrient: 'vertical',
+    display: '-webkit-box',
+  },
 
-const lineClamp = create({
-  base: { ...base, WebkitLineClamp: null },
-  1: { ...base, WebkitLineClamp: 1 },
-  2: { ...base, WebkitLineClamp: 2 },
-  3: { ...base, WebkitLineClamp: 3 },
-  4: { ...base, WebkitLineClamp: 4 },
-  5: { ...base, WebkitLineClamp: 5 },
-  6: { ...base, WebkitLineClamp: 6 },
-  none: {
+  notLineClamped: {
     overflow: 'visible',
     WebkitBoxOrient: 'horizontal',
     WebkitLineClamp: 'unset',
     display: 'block',
   },
 })
-
-//
