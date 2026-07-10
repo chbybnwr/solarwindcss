6b13a19a1b29d236a719b10a08b1260b7baa179b
Chabib N. <chabib.nr@gmail.com>
2026-07-10 16:08:51 +0700

    refactor: color scheme query

--- src/tokens/at-color-scheme.stylex.ts
+++ src/tokens/at-color-scheme.stylex.ts
@@ -3,7 +3,7 @@ export { atColorScheme }
 const atColorScheme = defineConsts({
   dark: '@media (prefers-color-scheme: dark)',
   light: '@media (prefers-color-scheme: light)',
-})
+} as const)
 
 import { defineConsts } from '@stylexjs/stylex'
 //
