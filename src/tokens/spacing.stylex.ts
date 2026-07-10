41f6d17a70688292f4e30f83e2ed017eb360a795
Chabib N. <chabib.nr@gmail.com>
2026-07-10 16:18:53 +0700

    refactor: spacing

--- src/tokens/spacing.stylex.ts
+++ src/tokens/spacing.stylex.ts
@@ -1,6 +1,6 @@
 export { spacing }
 
-const unit = '0.25rem'
+const base = '0.25rem'
 
 const spacing = defineConsts({
   /**
@@ -11,167 +11,167 @@ const spacing = defineConsts({
   /**
    * 0.125rem / ~2px
    */
-  0.5: `calc(${unit} * 0.5)`,
+  0.5: `calc(${base} * 0.5)`,
 
   /**
    * 0.25rem / ~4px
    */
-  1: `calc(${unit} * 1)`,
+  1: `calc(${base} * 1)`,
 
   /**
    * 0.375rem / ~6px
    */
-  1.5: `calc(${unit} * 1.5)`,
+  1.5: `calc(${base} * 1.5)`,
 
   /**
    * 0.5rem / ~8px
    */
-  2: `calc(${unit} * 2)`,
+  2: `calc(${base} * 2)`,
 
   /**
    * 0.625rem / ~10px
    */
-  2.5: `calc(${unit} * 2.5)`,
+  2.5: `calc(${base} * 2.5)`,
 
   /**
    * 0.75rem / ~12px
    */
-  3: `calc(${unit} * 3)`,
+  3: `calc(${base} * 3)`,
 
   /**
    * 0.875rem / ~14px
    */
-  3.5: `calc(${unit} * 3.5)`,
+  3.5: `calc(${base} * 3.5)`,
 
   /**
    * 1rem / ~16px
    */
-  4: `calc(${unit} * 4)`,
+  4: `calc(${base} * 4)`,
 
   /**
    * 1.25rem / ~20px
    */
-  5: `calc(${unit} * 5)`,
+  5: `calc(${base} * 5)`,
 
   /**
    * 1.5rem / ~24px
    */
-  6: `calc(${unit} * 6)`,
+  6: `calc(${base} * 6)`,
 
   /**
    * 1.75rem / ~28px
    */
-  7: `calc(${unit} * 7)`,
+  7: `calc(${base} * 7)`,
 
   /**
    * 2rem / ~32px
    */
-  8: `calc(${unit} * 8)`,
+  8: `calc(${base} * 8)`,
 
   /**
    * 2.25rem / ~36px
    */
-  9: `calc(${unit} * 9)`,
+  9: `calc(${base} * 9)`,
 
   /**
    * 2.5rem / ~40px
    */
-  10: `calc(${unit} * 10)`,
+  10: `calc(${base} * 10)`,
 
   /**
    * 2.75rem / ~44px
    */
-  11: `calc(${unit} * 11)`,
+  11: `calc(${base} * 11)`,
 
   /**
    * 3rem / ~48px
    */
-  12: `calc(${unit} * 12)`,
+  12: `calc(${base} * 12)`,
 
   /**
    * 3.5rem / ~56px
    */
-  14: `calc(${unit} * 14)`,
+  14: `calc(${base} * 14)`,
 
   /**
    * 4rem / ~64px
    */
-  16: `calc(${unit} * 16)`,
+  16: `calc(${base} * 16)`,
 
   /**
    * 5rem / ~80px
    */
-  20: `calc(${unit} * 20)`,
+  20: `calc(${base} * 20)`,
 
   /**
    * 6rem / ~96px
    */
-  24: `calc(${unit} * 24)`,
+  24: `calc(${base} * 24)`,
 
   /**
    * 7rem / ~112px
    */
-  28: `calc(${unit} * 28)`,
+  28: `calc(${base} * 28)`,
 
   /**
    * 8rem / ~128px
    */
-  32: `calc(${unit} * 32)`,
+  32: `calc(${base} * 32)`,
 
   /**
    * 9rem / ~144px
    */
-  36: `calc(${unit} * 36)`,
+  36: `calc(${base} * 36)`,
 
   /**
    * 10rem / ~160px
    */
-  40: `calc(${unit} * 40)`,
+  40: `calc(${base} * 40)`,
 
   /**
    * 11rem / ~176px
    */
-  44: `calc(${unit} * 44)`,
+  44: `calc(${base} * 44)`,
 
   /**
    * 12rem / ~192px
    */
-  48: `calc(${unit} * 48)`,
+  48: `calc(${base} * 48)`,
 
   /**
    * 13rem / ~208px
    */
-  52: `calc(${unit} * 52)`,
+  52: `calc(${base} * 52)`,
 
   /**
    * 14rem / ~224px
    */
-  56: `calc(${unit} * 56)`,
+  56: `calc(${base} * 56)`,
 
   /**
    * 15rem / ~240px
    */
-  60: `calc(${unit} * 60)`,
+  60: `calc(${base} * 60)`,
 
   /**
    * 16rem / ~256px
    */
-  64: `calc(${unit} * 64)`,
+  64: `calc(${base} * 64)`,
 
   /**
    * 18rem / ~288px
    */
-  72: `calc(${unit} * 72)`,
+  72: `calc(${base} * 72)`,
 
   /**
    * 20rem / ~320px
    */
-  80: `calc(${unit} * 80)`,
+  80: `calc(${base} * 80)`,
 
   /**
    * 24rem / ~384px
    */
-  96: `calc(${unit} * 96)`,
+  96: `calc(${base} * 96)`,
 } as const)
 
 import { defineConsts } from '@stylexjs/stylex'
