d972ee27c9c7a6fb51e003fa7f5d622fdfb4b8db
Chabib N. <chabib.nr@gmail.com>
2026-08-14 03:38:39 +0700

    refactor: text shadow
    - rename textShadowVars to textShadowVar
    - unify literal css variable names into constants

--- src/tokens/text-shadow.stylex.spec.ts
+++ src/tokens/text-shadow.stylex.spec.ts
@@ -4,9 +4,9 @@ describe('textShadow', () => {
   })
 })
 
-describe('textShadowVars', () => {
+describe('textShadowVar', () => {
   it('matches snapshot', () => {
-    expect(textShadowVars).toMatchSnapshot()
+    expect(textShadowVar).toMatchSnapshot()
   })
 })
 
@@ -14,5 +14,5 @@ import { describe } from 'vitest'
 import { expect } from 'vitest'
 import { it } from 'vitest'
 import { textShadow } from '#/tokens/text-shadow.stylex'
-import { textShadowVars } from '#/tokens/text-shadow.stylex'
+import { textShadowVar } from '#/tokens/text-shadow.stylex'
 //
--- src/tokens/text-shadow.stylex.ts
+++ src/tokens/text-shadow.stylex.ts
@@ -1,16 +1,18 @@
 export { textShadow }
-export { textShadowVars }
+export { textShadowVar }
 
-const textShadowVars = defineVars({
-  '--sw-text-shadow-color': null,
+const color = '--text-shadow-color'
+
+const textShadowVar = defineVars({
+  [color]: null,
 })
 
 const textShadow = defineConsts({
-  '2xs': `0px 1px 0px var(--sw-text-shadow-color, rgb(0 0 0 / 0.15))`,
-  xs: `0px 1px 1px var(--sw-text-shadow-color, rgb(0 0 0 / 0.2))`,
-  sm: `0px 1px 0px var(--sw-text-shadow-color, rgb(0 0 0 / 0.075)), 0px 1px 1px var(--sw-text-shadow-color, rgb(0 0 0 / 0.075)), 0px 2px 2px var(--sw-text-shadow-color, rgb(0 0 0 / 0.075))`,
-  md: `0px 1px 1px var(--sw-text-shadow-color, rgb(0 0 0 / 0.1)), 0px 1px 2px var(--sw-text-shadow-color, rgb(0 0 0 / 0.1)), 0px 2px 4px var(--sw-text-shadow-color, rgb(0 0 0 / 0.1))`,
-  lg: `0px 1px 2px var(--sw-text-shadow-color, rgb(0 0 0 / 0.1)), 0px 3px 2px var(--sw-text-shadow-color, rgb(0 0 0 / 0.1)), 0px 4px 8px var(--sw-text-shadow-color, rgb(0 0 0 / 0.1))`,
+  '2xs': `0px 1px 0px var(${color}, rgb(0 0 0 / 0.15))`,
+  xs: `0px 1px 1px var(${color}, rgb(0 0 0 / 0.2))`,
+  sm: `0px 1px 0px var(${color}, rgb(0 0 0 / 0.075)), 0px 1px 1px var(${color}, rgb(0 0 0 / 0.075)), 0px 2px 2px var(${color}, rgb(0 0 0 / 0.075))`,
+  md: `0px 1px 1px var(${color}, rgb(0 0 0 / 0.1)), 0px 1px 2px var(${color}, rgb(0 0 0 / 0.1)), 0px 2px 4px var(${color}, rgb(0 0 0 / 0.1))`,
+  lg: `0px 1px 2px var(${color}, rgb(0 0 0 / 0.1)), 0px 3px 2px var(${color}, rgb(0 0 0 / 0.1)), 0px 4px 8px var(${color}, rgb(0 0 0 / 0.1))`,
 } as const)
 
 import { defineConsts } from '@stylexjs/stylex'
